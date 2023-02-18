1b525cba7bbb7f496ba65117d71154cce94a8900
Quinn Dirks <qdirks@gmail.com>
2023-02-18 04:50:48 -0600

    save

--- packages/browser-sync/lib/logger.js
+++ packages/browser-sync/lib/logger.js
@@ -180,7 +180,9 @@ module.exports.callbacks = {
         function serveFiles(base) {
             const base_ = path.resolve(base);
             const parr = base_.split(path.sep);
-            if (parr.length > 2) base = parr[0] + path.sep + "~" + path.sep + parr[parr.length - 1];
+            const abbr = "<...>";
+            if (parr.length > 2 && parr.slice(1, -1).join(path.sep).length > abbr.length)
+                base = parr[0] + path.sep + abbr + path.sep + parr[parr.length - 1];
             else base = base_;
             logger.info("Serving files from: {magenta:%s}", base);
         }
